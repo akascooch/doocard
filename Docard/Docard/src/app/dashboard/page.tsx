37d456739c716c91bc79531e5f1c2df507605f22
Amir Ali Hosseini <a.hosseini@varanegar.local>
2025-07-16 15:33:47 +0330

    نسخه 1.0.0 - شروع رسمی پروژه

---
+++ Docard/Docard/src/app/dashboard/page.tsx
@@ -0,0 +1,4 @@
+import { Skeleton } from '@/components/ui/skeleton'
+import { UsersIcon, CalendarIcon, ScissorsIcon, ChartBarIcon } from '@heroicons/react/24/outline'
+
+const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'] 
